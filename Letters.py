b5d45d160a1c5f2c3e5448d534926da3faa0cb91
Gorea Gregoire <sineretsony@gmail.com>
2023-02-04 19:49:40 +0200

    Lessons_03

---
+++ Letters.py
@@ -0,0 +1,3 @@
+a = input("Введіть будь ласка рядок англійською: ")
+b = a.upper()
+print("Голосних букв : ", b.count("A") + b.count("E") + b.count("I") + b.count("O") + b.count("U") + b.count("Y"))
