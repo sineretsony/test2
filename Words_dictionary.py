e8ed13832e81d2997495125e965d387fcb20b454
Gorea Gregoire <sineretsony@gmail.com>
2023-02-21 23:08:52 +0200

    Homework_7_15

---
+++ Words_dictionary.py
@@ -0,0 +1,12 @@
+Line = ("One, two, three, one, two, five. Two, five.")
+dir_l = Line.lower().replace(",", " ").replace(".", " ").split()
+dict_list = {}
+word_i, calc_num = "", 0
+for i in dir_l:
+    n = dir_l.count(i)
+    dict_list[i] = n
+    if n > calc_num:
+        word_i, calc_num = i, n
+    elif n == calc_num and not word_i.count(i):
+        word_i += ", " + i
+print(dict_list, word_i)
