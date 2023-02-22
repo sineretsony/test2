ca5194a05c18df218628e6f1fe8c87d06ddc9770
Gorea Gregoire <sineretsony@gmail.com>
2023-02-22 20:26:17 +0200

    Homework_7_16_3

--- Words_dictionary.py
+++ Words_dictionary.py
@@ -1,7 +1,10 @@
+
 line = ("One, two, three, one, two, five. Two, five.")
+
 dir_l = line.lower().replace(",", " ").replace(".", " ").split()
 dict_list = {}
 word_i, calc_num = "", 0
+
 for i in dir_l:
     n = dir_l.count(i)
     dict_list[i] = n
@@ -9,4 +12,5 @@ for i in dir_l:
         word_i, calc_num = i, n
     elif n == calc_num and not word_i.count(i):
         word_i += ", " + i
+
 print(dict_list, word_i)
--- set_intersection.py
+++ set_intersection.py
@@ -1,7 +1,11 @@
+
 l1 = [1, 2, 2, 'three']
 l2 = [9, 8, 2, 'three']
+
 s3 = set(l1).intersection, set(l2)
 q = 0
+
 for i in s3:
     q += 1
+
 print("Кількість спільних елементів: ", q)
