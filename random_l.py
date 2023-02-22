62825642220701563dfa9120e3ccf32a0aac4e86
Gorea Gregoire <sineretsony@gmail.com>
2023-02-22 20:26:42 +0200

    Homework_7_16_4

--- random_l.py
+++ random_l.py
@@ -1,11 +1,14 @@
-import win32com.client
 
-# Создаем объект Illustrator
-app = win32com.client.Dispatch('Illustrator.Apres_plication')
+import random
 
-# Создаем новый документ
-doc = app.Documents.Add()
+random_num = [random.randint(1, 200) for i in range(10)]
+print(random_num)
 
-# Устанавливаем размер документа
-doc.Width = 94
-doc.Height = 54
+lon_ran = len(random_num) - 2
+
+for i in range(1, lon_ran):
+    buf1 = random_num[i]
+    buf2 = random_num[i + 1]
+    buf3 = random_num[i + 2]
+    if buf1 < buf2 > buf3:
+        print(buf2)
