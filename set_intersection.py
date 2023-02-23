7fd3ef9d74b334331059aec8f8897b3d4f243eb3
Gorea Gregoire <sineretsony@gmail.com>
2023-02-23 09:23:58 +0200

    Homework_7_16_6

--- set_intersection.py
+++ set_intersection.py
@@ -3,9 +3,6 @@ l1 = [1, 2, 2, 'three']
 l2 = [9, 8, 2, 'three']
 
 s3 = set(l1).intersection, set(l2)
-q = 0
-
-for i in s3:
-    q += 1
+q = len([i for i in s3])
 
 print("Кількість спільних елементів: ", q)
