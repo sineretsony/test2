b1f0ecd21fb70366eca531e267cb5942aa09cd61
Gorea Gregoire <sineretsony@gmail.com>
2023-02-23 09:17:19 +0200

    Homework_7_16_5

--- random_l.py
+++ random_l.py
@@ -1,14 +1,16 @@
 
 import random
 
-random_num = [random.randint(1, 200) for i in range(10)]
+random_num = tuple(random.randint(1, 200) for i in range(10))
 print(random_num)
 
 lon_ran = len(random_num) - 2
+calc = 0
 
 for i in range(1, lon_ran):
     buf1 = random_num[i]
     buf2 = random_num[i + 1]
     buf3 = random_num[i + 2]
     if buf1 < buf2 > buf3:
-        print(buf2)
+        calc += 1
+print(calc, "елементи більші за своїх сусідів")
