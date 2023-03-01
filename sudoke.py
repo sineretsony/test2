699967c466ea2d25d83cdcffd8470f83f1a803f5
Gorea Gregoire <sineretsony@gmail.com>
2023-03-01 15:56:46 +0200

    Homework_18_1

--- sudoke.py
+++ sudoke.py
@@ -8,30 +8,36 @@ m = [1, 7, 9, 3, 2, 8, 5, 6, 4,
      9, 4, 7, 5, 6, 2, 8, 3, 1,
      6, 1, 3, 9, 8, 7, 4, 2, 5]
 
+one, nine = 1, 9
+
 
 def get_row(n):
     row = [m[i:i + 9] for i in range(0, len(m), 9)]
-    print(f"Рядок   {n}", row[n - 1])
+    row_results = row[n - 1]
+    return row_results
 
 
 def get_column(n):
     column = [m[x::9] for x in range(len(m))]
-    print(f"Колонка {n}", column[n - 1])
+    col_results = column[n - 1]
+    return col_results
 
 
 def get_squares(n):
-    a = 0
     squares = []
+    a = 0
     while a <= 60:
         squares.append(m[a:a + 3] + m[a + 9:a + 12] + m[a + 18:a + 21])
         a += 3
         if a == 9 or a == 33:
             a += 18
-    print(f"Квадрат {n}", squares[n - 1])
+    squ_results = squares[n - 1]
+    return squ_results
 
-get_row(1)
-get_row(9)
-get_column(1)
-get_column(9)
-get_squares(1)
-get_squares(8)
+
+print(f"Рядок {one}", get_row(one),
+      f"\nРядок {nine}", get_row(nine))
+print(f"Колонка {one}", get_column(one),
+      f"\nКолонка {nine}", get_column(nine))
+print(f"Квадрат {one}", get_squares(one),
+      f"\nКвадрат {nine}", get_squares(nine))
