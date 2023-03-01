0cc2b81344edbd50e071cac2f4c1b06f62b973d1
Gorea Gregoire <sineretsony@gmail.com>
2023-03-01 10:34:56 +0200

    Homework_20_2

--- old_movie.py
+++ old_movie.py
@@ -1,18 +1,14 @@
-# import datetime
 d = "Casablanca 1942, Gone with the Wind 1939, The Godfather 1972, Psycho 1960"
 
 list_now = d.split(",")
-year = 2023  # datetime.datetime.now().year
-
 dictionary = {}
-results = ""
 
 for i in list_now:
     temp = i.strip().rsplit(" ", 1)
     dictionary[temp[0]] = int(temp[1])
-    if int(temp[1]) < year:
-        year = int(temp[1])
-        results = temp[0]
+
+ol_mov = min(dictionary.items(), key=lambda x: x[1])[0]
 
 print(dictionary)
-print("Найстаріший фільм:", results)
+print("Найстаріший фільм:", ol_mov)
+
