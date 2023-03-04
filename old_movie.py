9ebe72515f9e0681159a543e33215e82a2b54e44
Gorea Gregoire <sineretsony@gmail.com>
2023-03-04 18:47:46 +0200

    Homework_003

--- old_movie.py
+++ old_movie.py
@@ -2,23 +2,19 @@ d = "Casablanca 1942, Gone with the Wind 1939, The Godfather 1972, Psycho 1960"
 
 list_now = d.split(",")
 
-dictionary = {}
-results = ""
-year = 0
-
-
 def fil_mov():
-    global results, year, dictionary
+    dictionary = {}
+    min_year = None
+    oldest_movie = ""
     for i in list_now:
         temp = i.strip().rsplit(" ", 1)
         dictionary[temp[0]] = int(temp[1])
-        if int(temp[1]) < year or year == 0:
-            year = int(temp[1])
-            results = temp[0]
-    return results
+        year = int(temp[1])
+        if min_year is None or year < min_year:
+            min_year = year
+            oldest_movie = temp[0]
+    results = f"Найстаріший фільм: {oldest_movie}"
+    return dictionary, results
 
-
-fil_mov()
-
-print(dictionary)
-print("Найстаріший фільм:", results)
+result = fil_mov()
+print(result[0], result[1])
