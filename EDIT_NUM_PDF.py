598c70d8229c54348401c7c92c1dcdd397f85e4c
Gorea Gregoire <sineretsony@gmail.com>
2024-02-17 21:54:33 +0200

    mini fix

--- EDIT_NUM_PDF.py
+++ EDIT_NUM_PDF.py
@@ -12,16 +12,17 @@ with open(input_pdf_path, "rb") as input_pdf_file:
 
     # Копируем содержимое исходного файла в новый файл
     for page_num in range(len(pdf_reader.pages)):
-        output_pdf.add_page(pdf_reader.pages[page_num])
-
-    # Добавляем пустую страницу
-    output_pdf.add_blank_page()
+        page = pdf_reader.pages[page_num]
+        # Делаем небольшое смещение
+        upper_right_x, upper_right_y = page.mediabox.upper_right
+        page.mediabox.upper_right = (upper_right_x - 1, upper_right_y)
+        output_pdf.add_page(page)
 
     # Путь для сохранения измененного файла
-    output_pdf_path = r"C:\Users\gorea\Desktop\Новая папка\file_with_blank_page.pdf"
+    output_pdf_path = r"C:\Users\gorea\Desktop\Новая папка\file_with_offset.pdf"
 
     # Открываем файл для записи
     with open(output_pdf_path, "wb") as output_pdf_file:
         output_pdf.write(output_pdf_file)
 
-print("Добавлена пустая страница в PDF-файл.")
+print("Выполнено небольшое смещение первой страницы в PDF-файле.")
