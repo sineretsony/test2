abce674136782fd034f861a52e946c4b5b07e254
Gorea Gregoire <sineretsony@gmail.com>
2024-02-17 22:32:46 +0200

    add text

--- EDIT_NUM_PDF.py
+++ EDIT_NUM_PDF.py
@@ -9,15 +9,17 @@ pdf_document = fitz.open(input_pdf_path)
 # Получаем первую страницу
 page = pdf_document[0]
 
-# Добавляем прямоугольник 1x1 пиксель белого цвета в левом верхнем углу
-rect = fitz.Rect(0, page.rect.height, 1, page.rect.height - 1)  # x0, y0, x1, y1
-page.draw_rect(rect, fill=(1, 1, 1))  # fill=(R, G, B)
+# Добавляем текст "2347" красным цветом в левый верхний угол
+text = "2347"
+font_size = 24
+font_color = (1, 0, 0)  # Красный цвет
+page.insert_text((10, 10), text, fontsize=font_size, color=font_color)
 
 # Сохраняем изменения
-output_pdf_path = r"C:\Users\gorea\Desktop\Новая папка\file_with_pixel.pdf"
+output_pdf_path = r"C:\Users\gorea\Desktop\Новая папка\file_with_text.pdf"
 pdf_document.save(output_pdf_path)
 
 # Закрываем PDF-файл
 pdf_document.close()
 
-print("Добавлена точка 1x1 пиксель белого цвета в левый верхний угол первой страницы PDF-файла.")
+print("Добавлен текст '2347' красным цветом в левый верхний угол первой страницы PDF-файла.")
